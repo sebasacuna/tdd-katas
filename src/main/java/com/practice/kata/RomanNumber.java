26faa82fe2d47317874b14d2f0871749f693930b
sebastianit <sebastianacuna.it@gmail.com>
2020-10-25 11:30:39 -0300

    refactor -add micro refactor extract method to separate logic of convertSingle Digit to arabic number in a single method

--- src/main/java/com/practice/kata/RomanNumber.java
+++ src/main/java/com/practice/kata/RomanNumber.java
@@ -4,22 +4,7 @@ public class RomanNumber {
 
     public static int convertToArabicNumber(String romanNumber) throws Exception {
         if (romanNumber.length() == 1) {
-            switch (romanNumber) {
-                case "I":
-                    return 1;
-                case "V":
-                    return 5;
-                case "X":
-                    return 10;
-                case "C":
-                    return 100;
-                case "D":
-                    return 500;
-                case "L":
-                    return 50;
-                case "M":
-                    return 1000;
-            }
+            return convertSingleDigitToArabicNumber(romanNumber);
         }
         if (romanNumber.equals("II")) {
             return 2;
@@ -27,6 +12,26 @@ public class RomanNumber {
             return 3;
         } else if (romanNumber.equals("VI")) {
             return 6;
+        }
+        throw new Exception("the number do not exist");
+    }
+
+    private static int convertSingleDigitToArabicNumber(String romanNumber) throws Exception {
+        switch (romanNumber) {
+            case "I":
+                return 1;
+            case "V":
+                return 5;
+            case "X":
+                return 10;
+            case "C":
+                return 100;
+            case "D":
+                return 500;
+            case "L":
+                return 50;
+            case "M":
+                return 1000;
         }
         throw new Exception("the number do not exist");
     }
