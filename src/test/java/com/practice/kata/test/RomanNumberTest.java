3555fd8b170130244e133313d32f49cb43a12362
sebastianit <sebastianacuna.it@gmail.com>
2020-10-25 11:14:19 -0300

    test - refactor old test and add new test to convert roman numbers with one digit

--- src/test/java/com/practice/kata/test/RomanNumberTest.java
+++ src/test/java/com/practice/kata/test/RomanNumberTest.java
@@ -36,27 +36,25 @@ public class RomanNumberTest {
     }
 
     @Test()
-    public void convertManyRomanNumbersToArabicNumber() throws Exception {
-        assertEquals(1, RomanNumber.convertToArabicNumber("I"));
-        assertEquals(2, RomanNumber.convertToArabicNumber("II"));
-        assertEquals(3, RomanNumber.convertToArabicNumber("III"));
-        assertEquals(4, RomanNumber.convertToArabicNumber("IV"));
-        assertEquals(5, RomanNumber.convertToArabicNumber("V"));
-        assertEquals(6, RomanNumber.convertToArabicNumber("VI"));
-        assertEquals(9, RomanNumber.convertToArabicNumber("IX"));
-        assertEquals(13, RomanNumber.convertToArabicNumber("XIII"));
-        assertEquals(50, RomanNumber.convertToArabicNumber("L"));
-        assertEquals(100, RomanNumber.convertToArabicNumber("C"));
-        assertEquals(500, RomanNumber.convertToArabicNumber("D"));
-        assertEquals(100, RomanNumber.convertToArabicNumber("M"));
-
-    }
-
-    @Test()
     public void romanNumeralAddition() throws Exception {
         assertEquals(2, RomanNumber.convertToArabicNumber("II"));
         assertEquals(3, RomanNumber.convertToArabicNumber("III"));
         assertEquals(6, RomanNumber.convertToArabicNumber("VI"));
     }
+
+    @Test()
+    public void convertRomanNumbersWithOneDigits() throws Exception {
+        assertEquals(1, RomanNumber.convertToArabicNumber("I"));
+        assertEquals(1, RomanNumber.convertToArabicNumber("V"));
+        assertEquals(1, RomanNumber.convertToArabicNumber("X"));
+        assertEquals(1, RomanNumber.convertToArabicNumber("V"));
+        assertEquals(1, RomanNumber.convertToArabicNumber("X"));
+        assertEquals(1, RomanNumber.convertToArabicNumber("L"));
+        assertEquals(1, RomanNumber.convertToArabicNumber("C"));
+        assertEquals(1, RomanNumber.convertToArabicNumber("D"));
+        assertEquals(1, RomanNumber.convertToArabicNumber("M"));
+    }
+
+
 
 }
