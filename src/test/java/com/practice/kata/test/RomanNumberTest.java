c3aaa86f79efe9c5c1f16f32c9e4ac28f8aa3759
sebastianit <sebastianacuna.it@gmail.com>
2020-10-25 03:56:26 -0300

    test - add new method convert to make the test fail

--- src/test/java/com/practice/kata/test/RomanNumberTest.java
+++ src/test/java/com/practice/kata/test/RomanNumberTest.java
@@ -2,6 +2,7 @@ package com.practice.kata.test;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RomanNumberTest {
@@ -9,6 +10,16 @@ public class RomanNumberTest {
     @Test
     public void junitWorks(){
         assertTrue(true);
+    }
+
+    @Test
+    public void convertSingleDigitRoman(){
+        /*
+         the code will not compile, in consequence the test will fail that it is
+         good because indicate that the feature not exist yet
+        */
+        final int arabic = convert("I");
+        assertEquals(1,arabic);
     }
 
 }
