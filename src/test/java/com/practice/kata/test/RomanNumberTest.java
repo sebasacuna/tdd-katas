f92480baf5dd49ef971c7f3978bac3b784338e9a
sebastianit <sebastianacuna.it@gmail.com>
2020-10-25 03:49:21 -0300

    feature - add new test to make sure it fails

---
+++ src/test/java/com/practice/kata/test/RomanNumberTest.java
@@ -0,0 +1,14 @@
+package com.practice.kata.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RomanNumberTest {
+
+    @Test
+    public void junitWorks(){
+        assertTrue(false);
+    }
+
+}
