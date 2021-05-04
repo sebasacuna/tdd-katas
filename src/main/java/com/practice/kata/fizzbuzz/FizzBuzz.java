410cf962f1fd7d80510b9a1cff10ffc184057e8f
Sebastian <sebastianacuna.it@gmail.com>
2021-05-03 20:08:21 -0400

    [feature/fizz-buzz] (test pass) : make the test pass

--- src/main/java/com/practice/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/practice/kata/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,7 @@ package com.practice.kata.fizzbuzz;
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number){
+        if (number % 5 == 0 && number % 3 == 0) return "FizzBuzz";
         if (number % 5 == 0) return "Buzz";
         if (number % 3 == 0) return "Fizz";
         return "";
