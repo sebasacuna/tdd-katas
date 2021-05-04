0908d08cdcbf0e5b04299b9006f4f9c5a4e01915
Sebastian <sebastianacuna.it@gmail.com>
2021-05-03 20:04:10 -0400

    [feature/fizz-buzz] (refactor) : refactor buzz case

--- src/main/java/com/practice/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/practice/kata/fizzbuzz/FizzBuzz.java
@@ -3,8 +3,8 @@ package com.practice.kata.fizzbuzz;
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number){
+        if (number % 5 == 0) return "Buzz";
         if (number % 3 == 0) return "Fizz";
-        if (number.equals(5)) return "Buzz";
         return "";
     }
 
