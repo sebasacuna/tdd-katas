50683b3ae4ab429ae16ce7f1c0a2c9e05fd45414
Sebastian <sebastianacuna.it@gmail.com>
2021-05-03 20:12:06 -0400

    [feature/fizz-buzz] (refactor) : refactor

--- src/main/java/com/practice/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/practice/kata/fizzbuzz/FizzBuzz.java
@@ -2,11 +2,30 @@ package com.practice.kata.fizzbuzz;
 
 public class FizzBuzz {
 
-    public static String fizzBuzz(Integer number){
-        if (number % 5 == 0 && number % 3 == 0) return "FizzBuzz";
-        if (number % 5 == 0) return "Buzz";
-        if (number % 3 == 0) return "Fizz";
+    public static String fizzBuzz(Integer number) {
+
+        if (numberIsMultipleOfFive(number) && numberIsMultipleOfThree(number)) {
+            return "FizzBuzz";
+        }
+
+        if (numberIsMultipleOfFive(number)) {
+            return "Buzz";
+        }
+
+        if (numberIsMultipleOfThree(number)) {
+            return "Fizz";
+        }
+
         return "";
     }
+
+    private static boolean numberIsMultipleOfThree(Integer number) {
+        return (number % 3 == 0);
+    }
+
+    private static boolean numberIsMultipleOfFive(Integer number) {
+        return (number % 5 == 0);
+    }
+
 
 }
